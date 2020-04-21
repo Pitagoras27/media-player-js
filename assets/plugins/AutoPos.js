d3dd96272e2f6699f726a28c18522154c745cf04
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-20 22:12:05 -0500

    Se trata de implementar visibilitychange pero se tienen más problemas con this, se refactorizará esta clase de prototipo por una clase de ES6

--- assets/plugins/AutoPos.js
+++ assets/plugins/AutoPos.js
@@ -2,6 +2,9 @@ function AutoPos(media) {
     this.threshold = 0.25
     this.player = media
     this.handleIntersection = this.handleIntersection.bind(this)
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
+
 }
 
 AutoPos.prototype.run = function (player) {
@@ -12,6 +15,15 @@ AutoPos.prototype.run = function (player) {
     })
 
     observer.observe(this.player.media)
+    console.log("asdasdasdasd")
+    // document.addEventListener('visibilitychange', () => this.handleVisibilityChange)
+    document.addEventListener("visibilitychange", function () {
+        if (document.visibilityState === 'visible') {
+            console.log("visible", player)
+        } else {
+            console.log("hidden")
+        }
+    });
 }
 
 AutoPos.prototype.handleIntersection = function (entries) {
@@ -21,4 +33,9 @@ AutoPos.prototype.handleIntersection = function (entries) {
     isVisible ? this.player.play() : this.player.pause();
 }
 
+AutoPos.prototype.handleVisibilityChange = function () {
+    const isVisible = document.visibilityState === 'visible'
+    console.log(document.visibilityState)
+    isVisible ? 'this.player.play()' : 'this.player.pause()'
+}
 export default AutoPos
