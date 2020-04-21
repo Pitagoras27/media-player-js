8e252689a6dad1e94464d972c942f54168aff425
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-21 17:40:29 -0500

    Sevice worker

--- assets/index.js
+++ assets/index.js
@@ -22,3 +22,9 @@ button.onclick = () => {
 muted.onclick = () => {
     mediaPlayer.toogleMuted()
 }
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+        console.log(error.message)
+    })
+}
